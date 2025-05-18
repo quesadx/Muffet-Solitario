49603fc72844acde2938d5097f51611912e1b50c
Alex Zúñiga <alex.zuniga.monge@est.una.ac.cr>
2025-05-18 16:08:32 -0600

    feat: EntityManagerHelper class added

---
+++ src/main/java/cr/ac/una/muffetsolitario/util/EntityManagerHelper.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cr.ac.una.muffetsolitario.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerHelper {
+
+    private static final EntityManagerHelper SINGLENTON = new EntityManagerHelper();
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
+
+    static {
+        try {
+            emf = Persistence.createEntityManagerFactory("UnaPlanillaPU");
+            em = emf.createEntityManager();
+        } catch (ExceptionInInitializerError e) {
+            throw e;
+        }
+    }
+    
+    public static EntityManagerHelper getInstance() {
+
+        return SINGLENTON;
+    }
+
+    public static EntityManager getManager() {
+        if (em == null) {
+            emf = Persistence.createEntityManagerFactory("MuffetSolitarioPU");
+            em = emf.createEntityManager();
+        }
+        return em;
+    }
+}
