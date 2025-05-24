bdf5dd53370a7c26f2104b5bc3d6d4e18bdf0e10
Alex Zúñiga <alex.zuniga.monge@est.una.ac.cr>
2025-05-23 23:08:03 -0600

    feat: add GameDto and CardDto classes for data transfer

---
+++ src/main/java/cr/ac/una/muffetsolitario/model/CardDto.java
@@ -0,0 +1,166 @@
+package cr.ac.una.muffetsolitario.model;
+
+import javafx.beans.property.*;
+
+public class CardDto {
+    
+    private LongProperty cardId;
+    private BooleanProperty cardFaceUp;
+    private StringProperty cardSuit;
+    private IntegerProperty cardValue;
+    private IntegerProperty cardPositionInContainer;
+    
+    private Long cardBcolmnId;
+    private Long cardCseqId;
+    private Long cardDeckId;
+    
+    private BooleanProperty selected;
+    private BooleanProperty draggable;
+    private BooleanProperty highlighted;
+    
+    public CardDto() {
+        this.cardId = new SimpleLongProperty();
+        this.cardFaceUp = new SimpleBooleanProperty();
+        this.cardSuit = new SimpleStringProperty();
+        this.cardValue = new SimpleIntegerProperty();
+        this.cardPositionInContainer = new SimpleIntegerProperty();
+        this.selected = new SimpleBooleanProperty(false);
+        this.draggable = new SimpleBooleanProperty(true);
+        this.highlighted = new SimpleBooleanProperty(false);
+    }
+    
+    public CardDto(Long cardId, boolean cardFaceUp, String cardSuit, Integer cardValue, Integer cardPositionInContainer) {
+        this();
+        setCardId(cardId);
+        setCardFaceUp(cardFaceUp);
+        setCardSuit(cardSuit);
+        setCardValue(cardValue);
+        setCardPositionInContainer(cardPositionInContainer);
+    }
+    
+    public LongProperty cardIdProperty() {
+        return cardId;
+    }
+    
+    public Long getCardId() {
+        return cardId.get();
+    }
+    
+    public void setCardId(Long cardId) {
+        this.cardId.set(cardId);
+    }
+    
+    public BooleanProperty cardFaceUpProperty() {
+        return cardFaceUp;
+    }
+    
+    public boolean isCardFaceUp() {
+        return cardFaceUp.get();
+    }
+    
+    public void setCardFaceUp(boolean cardFaceUp) {
+        this.cardFaceUp.set(cardFaceUp);
+    }
+    
+    public StringProperty cardSuitProperty() {
+        return cardSuit;
+    }
+    
+    public String getCardSuit() {
+        return cardSuit.get();
+    }
+    
+    public void setCardSuit(String cardSuit) {
+        this.cardSuit.set(cardSuit);
+    }
+    
+    public IntegerProperty cardValueProperty() {
+        return cardValue;
+    }
+    
+    public Integer getCardValue() {
+        return cardValue.get();
+    }
+    
+    public void setCardValue(Integer cardValue) {
+        this.cardValue.set(cardValue);
+    }
+    
+    public IntegerProperty cardPositionInContainerProperty() {
+        return cardPositionInContainer;
+    }
+    
+    public Integer getCardPositionInContainer() {
+        return cardPositionInContainer.get();
+    }
+    
+    public void setCardPositionInContainer(Integer cardPositionInContainer) {
+        this.cardPositionInContainer.set(cardPositionInContainer);
+    }
+    
+    // Properties only for UI
+    public BooleanProperty selectedProperty() {
+        return selected;
+    }
+    
+    public boolean isSelected() {
+        return selected.get();
+    }
+    
+    public void setSelected(boolean selected) {
+        this.selected.set(selected);
+    }
+    
+    public BooleanProperty draggableProperty() {
+        return draggable;
+    }
+    
+    public boolean isDraggable() {
+        return draggable.get();
+    }
+    
+    public void setDraggable(boolean draggable) {
+        this.draggable.set(draggable);
+    }
+    
+    public BooleanProperty highlightedProperty() {
+        return highlighted;
+    }
+    
+    public boolean isHighlighted() {
+        return highlighted.get();
+    }
+    
+    public void setHighlighted(boolean highlighted) {
+        this.highlighted.set(highlighted);
+    }
+    
+    public Long getCardBcolmnId() {
+        return cardBcolmnId;
+    }
+    
+    public void setCardBcolmnId(Long cardBcolmnId) {
+        this.cardBcolmnId = cardBcolmnId;
+    }
+    
+    public Long getCardCseqId() {
+        return cardCseqId;
+    }
+    
+    public void setCardCseqId(Long cardCseqId) {
+        this.cardCseqId = cardCseqId;
+    }
+    
+    public Long getCardDeckId() {
+        return cardDeckId;
+    }
+    
+    public void setCardDeckId(Long cardDeckId) {
+        this.cardDeckId = cardDeckId;
+    }
+    
+    @Override
+    public String toString() {
+        return "CardDto{" + "id=" + cardId + "value=" + cardValue + "suit=" + cardSuit + "}";
+    }
+}
---
+++ src/main/java/cr/ac/una/muffetsolitario/model/GameDto.java
@@ -0,0 +1,215 @@
+package cr.ac.una.muffetsolitario.model;
+
+import javafx.beans.property.*;
+import java.time.LocalDate;
+
+public class GameDto {
+    
+    private LongProperty gameId;
+    private IntegerProperty gameCompletedSequences;
+    private IntegerProperty gameDealsRemaining;
+    private IntegerProperty gameMoveCount;
+    private StringProperty gameDifficulty;
+    private IntegerProperty gameDurationSeconds;
+    private IntegerProperty gameTotalPoints;
+    private StringProperty gameStatus;
+    private ObjectProperty<LocalDate> gameCreatedDate;
+    private ObjectProperty<LocalDate> gameLastPlayed;
+    private Long gameUserFk;
+    
+    public GameDto() {
+        this.gameId = new SimpleLongProperty();
+        this.gameCompletedSequences = new SimpleIntegerProperty(0);
+        this.gameDealsRemaining = new SimpleIntegerProperty(0);
+        this.gameMoveCount = new SimpleIntegerProperty(0);
+        this.gameDifficulty = new SimpleStringProperty();
+        this.gameDurationSeconds = new SimpleIntegerProperty(0);
+        this.gameTotalPoints = new SimpleIntegerProperty(0);
+        this.gameStatus = new SimpleStringProperty();
+        this.gameCreatedDate = new SimpleObjectProperty<>();
+        this.gameLastPlayed = new SimpleObjectProperty<>();
+    }
+    
+    public GameDto(Long gameId, Integer gameMoveCount, String gameDifficulty, 
+                   Integer gameDurationSeconds, Integer gameTotalPoints, String gameStatus) {
+        this();
+        setGameId(gameId);
+        setGameMoveCount(gameMoveCount);
+        setGameDifficulty(gameDifficulty);
+        setGameDurationSeconds(gameDurationSeconds);
+        setGameTotalPoints(gameTotalPoints);
+        setGameStatus(gameStatus);
+    }
+    
+    public LongProperty gameIdProperty() {
+        return gameId;
+    }
+    
+    public Long getGameId() {
+        return gameId.get();
+    }
+    
+    public void setGameId(Long gameId) {
+        this.gameId.set(gameId);
+    }
+    
+    public IntegerProperty gameCompletedSequencesProperty() {
+        return gameCompletedSequences;
+    }
+    
+    public Integer getGameCompletedSequences() {
+        return gameCompletedSequences.get();
+    }
+    
+    public void setGameCompletedSequences(Integer gameCompletedSequences) {
+        this.gameCompletedSequences.set(gameCompletedSequences != null ? gameCompletedSequences : 0);
+    }
+    
+    public IntegerProperty gameDealsRemainingProperty() {
+        return gameDealsRemaining;
+    }
+    
+    public Integer getGameDealsRemaining() {
+        return gameDealsRemaining.get();
+    }
+    
+    public void setGameDealsRemaining(Integer gameDealsRemaining) {
+        this.gameDealsRemaining.set(gameDealsRemaining != null ? gameDealsRemaining : 0);
+    }
+    
+    public IntegerProperty gameMoveCountProperty() {
+        return gameMoveCount;
+    }
+    
+    public Integer getGameMoveCount() {
+        return gameMoveCount.get();
+    }
+    
+    public void setGameMoveCount(Integer gameMoveCount) {
+        this.gameMoveCount.set(gameMoveCount);
+    }
+    
+    public StringProperty gameDifficultyProperty() {
+        return gameDifficulty;
+    }
+    
+    public String getGameDifficulty() {
+        return gameDifficulty.get();
+    }
+    
+    public void setGameDifficulty(String gameDifficulty) {
+        this.gameDifficulty.set(gameDifficulty);
+    }
+    
+    public IntegerProperty gameDurationSecondsProperty() {
+        return gameDurationSeconds;
+    }
+    
+    public Integer getGameDurationSeconds() {
+        return gameDurationSeconds.get();
+    }
+    
+    public void setGameDurationSeconds(Integer gameDurationSeconds) {
+        this.gameDurationSeconds.set(gameDurationSeconds);
+    }
+    
+    public IntegerProperty gameTotalPointsProperty() {
+        return gameTotalPoints;
+    }
+    
+    public Integer getGameTotalPoints() {
+        return gameTotalPoints.get();
+    }
+    
+    public void setGameTotalPoints(Integer gameTotalPoints) {
+        this.gameTotalPoints.set(gameTotalPoints);
+    }
+    
+    public StringProperty gameStatusProperty() {
+        return gameStatus;
+    }
+    
+    public String getGameStatus() {
+        return gameStatus.get();
+    }
+    
+    public void setGameStatus(String gameStatus) {
+        this.gameStatus.set(gameStatus);
+    }
+    
+    public ObjectProperty<LocalDate> gameCreatedDateProperty() {
+        return gameCreatedDate;
+    }
+    
+    public LocalDate getGameCreatedDate() {
+        return gameCreatedDate.get();
+    }
+    
+    public void setGameCreatedDate(LocalDate gameCreatedDate) {
+        this.gameCreatedDate.set(gameCreatedDate);
+    }
+    
+    public ObjectProperty<LocalDate> gameLastPlayedProperty() {
+        return gameLastPlayed;
+    }
+    
+    public LocalDate getGameLastPlayed() {
+        return gameLastPlayed.get();
+    }
+    
+    public void setGameLastPlayed(LocalDate gameLastPlayed) {
+        this.gameLastPlayed.set(gameLastPlayed);
+    }
+    
+    public Long getGameUserFk() {
+        return gameUserFk;
+    }
+    
+    public void setGameUserFk(Long gameUserFk) {
+        this.gameUserFk = gameUserFk;
+    }
+    
+    public String getFormattedDuration() {
+        int totalSeconds = getGameDurationSeconds();
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+    
+   public boolean isGameInProgress() {
+        return "ON".equals(getGameStatus());
+    }
+    
+    public boolean isGameCompleted() {
+        return "WON".equals(getGameStatus()) || "LOST".equals(getGameStatus());
+    }
+    
+    public boolean isGamePaused() {
+        return "SAVED".equals(getGameStatus());
+    }
+    
+    public double getCompletionPercentage() {
+        return (getGameCompletedSequences() / 8.0) * 100.0;
+    }
+    
+    public void incrementMoveCount() {
+        setGameMoveCount(getGameMoveCount() + 1);
+    }
+    
+    public void incrementCompletedSequences() {
+        setGameCompletedSequences(getGameCompletedSequences() + 1);
+    }
+    
+    public void decrementDealsRemaining() {
+        if (getGameDealsRemaining() > 0) {
+            setGameDealsRemaining(getGameDealsRemaining() - 1);
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return "GameDto{" + "id=" + gameId + ", status=" + gameStatus + 
+               ", moves=" + gameMoveCount + ", points=" + gameTotalPoints + 
+               ", duration=" + getFormattedDuration() + "}";
+    }
+}
