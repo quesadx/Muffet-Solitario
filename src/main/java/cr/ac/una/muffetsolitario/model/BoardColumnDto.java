fa3d1b321261df05a7a2cf7fe5642db7675e163c
Alex Zúñiga <alex.zuniga.monge@est.una.ac.cr>
2025-05-24 20:12:45 -0600

    feat: add methods to BoardColumnDto and DeckDto

--- src/main/java/cr/ac/una/muffetsolitario/model/BoardColumnDto.java
+++ src/main/java/cr/ac/una/muffetsolitario/model/BoardColumnDto.java
@@ -4,10 +4,119 @@
  */
 package cr.ac.una.muffetsolitario.model;
 
-/**
- *
- * @author Al3xz
- */
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class BoardColumnDto {
+   
+    private LongProperty bcolmnId;
+    private IntegerProperty bcolmnIndex; 
+    private Long bcolmnGameFk;
+    private ObservableList<CardDto> cardList;
+    //the type could change to CardContainer instead of CardDto -> @Kendall!!
+    //Or just added next to it as cardViews
     
+    public BoardColumnDto() {
+        this.bcolmnId = new SimpleLongProperty();
+        this.bcolmnIndex = new SimpleIntegerProperty();
+        this.cardList = FXCollections.observableArrayList();
+    }
+    
+    public BoardColumnDto(Long bcolmnId, Integer bcolmnIndex) {
+        this();
+        setBcolmnId(bcolmnId);
+        setBcolmnIndex(bcolmnIndex);
+    }
+    
+    // Getters y Setters para Properties
+    public LongProperty bcolmnIdProperty() {
+        return bcolmnId;
+    }
+    
+    public Long getBcolmnId() {
+        return bcolmnId.get();
+    }
+    
+    public void setBcolmnId(Long bcolmnId) {
+        this.bcolmnId.set(bcolmnId);
+    }
+    
+    public IntegerProperty bcolmnIndexProperty() {
+        return bcolmnIndex;
+    }
+    
+    public Integer getBcolmnIndex() {
+        return bcolmnIndex.get();
+    }
+    
+    public void setBcolmnIndex(Integer bcolmnIndex) {
+        this.bcolmnIndex.set(bcolmnIndex);
+    }
+    
+    // Getter y Setter simple para ID de relación
+    public Long getBcolmnGameFk() {
+        return bcolmnGameFk;
+    }
+    
+    public void setBcolmnGameFk(Long bcolmnGameFk) {
+        this.bcolmnGameFk = bcolmnGameFk;
+    }
+    
+    // Getter y Setter para lista observable de cartas
+    public ObservableList<CardDto> getCardList() {
+        return cardList;
+    }
+    
+    public void setCardList(ObservableList<CardDto> cardList) {
+        this.cardList = cardList;
+    }
+    
+    //methods useful for game UI data
+    public boolean isEmpty() {
+        return cardList == null || cardList.isEmpty();
+    }
+    
+    public int getCardCount() {
+        return cardList != null ? cardList.size() : 0;
+    }
+    
+    public CardDto getTopCard() {
+        if (isEmpty()) {
+            return null;
+        }
+        return cardList.get(cardList.size() - 1);
+    }
+    
+    public CardDto getBottomCard() {
+        if (isEmpty()) {
+            return null;
+        }
+        return cardList.get(0);
+    }
+    
+    public void addCard(CardDto card) {
+        if (cardList != null && card != null) {
+            cardList.add(card);
+        }
+    }
+    
+    public CardDto removeTopCard() {
+        if (isEmpty()) {
+            return null;
+        }
+        return cardList.remove(cardList.size() - 1);
+    }
+    
+    public void clearColumn() {
+        if (cardList != null) {
+            cardList.clear();
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return "BoardColumnDTO{" + "id=" + getBcolmnId() + ", index=" + getBcolmnIndex() + 
+               ", cardCount=" + getCardCount() + "}";
+    }
 }
--- src/main/java/cr/ac/una/muffetsolitario/model/DeckDto.java
+++ src/main/java/cr/ac/una/muffetsolitario/model/DeckDto.java
@@ -4,10 +4,87 @@
  */
 package cr.ac.una.muffetsolitario.model;
 
-/**
- *
- * @author Al3xz
- */
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class DeckDto {
     
+    private LongProperty deckId;
+    private Long deckGameId;
+    private ObservableList<CardDto> cardList;
+    //Also could change to type CardContainer instead of CardDto
+    
+    public DeckDto() {
+        this.deckId = new SimpleLongProperty();
+        this.cardList = FXCollections.observableArrayList();
+    }
+    
+    public DeckDto(Long deckId) {
+        this();
+        setDeckId(deckId);
+    }
+    
+    public DeckDto(Long deckId, Long deckGameId) {
+        this();
+        setDeckId(deckId);
+        setDeckGameId(deckGameId);
+    }
+    
+    public LongProperty deckIdProperty() {
+        return deckId;
+    }
+    
+    public Long getDeckId() {
+        return deckId.get();
+    }
+    
+    public void setDeckId(Long deckId) {
+        this.deckId.set(deckId);
+    }
+    
+    public Long getDeckGameId() {
+        return deckGameId;
+    }
+    
+    public void setDeckGameId(Long deckGameId) {
+        this.deckGameId = deckGameId;
+    }
+    
+    public ObservableList<CardDto> getCardList() {
+        return cardList;
+    }
+    
+    public void setCardList(ObservableList<CardDto> cardList) {
+        this.cardList = cardList;
+    }
+    
+    // Utility methods for card management
+    public void addCard(CardDto card) {
+        if (card != null) {
+            card.setCardDeckId(this.getDeckId());
+            this.cardList.add(card);
+        }
+    }
+    
+    public void removeCard(CardDto card) {
+        this.cardList.remove(card);
+    }
+    
+    public void clearCards() {
+        this.cardList.clear();
+    }
+    
+    public int getCardCount() {
+        return cardList.size();
+    }
+    
+    public boolean isEmpty() {
+        return cardList.isEmpty();
+    }
+    
+    @Override
+    public String toString() {
+        return "DeckDto{" + "id=" + deckId + ", gameId=" + deckGameId + ", cardCount=" + getCardCount() + "}";
+    }
 }
